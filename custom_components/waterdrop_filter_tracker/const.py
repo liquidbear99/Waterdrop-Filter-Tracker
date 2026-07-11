f94e420a5d1f168210d11b17f95012c10dda298a
liquidbear99 <53712660+liquidbear99@users.noreply.github.com>
2026-07-11 12:57:18 -0500

    Create custom_components/waterdrop_filter_tracker/const.py

---
+++ custom_components/waterdrop_filter_tracker/const.py
@@ -0,0 +1,14 @@
+"""Constants for Waterdrop Filter Tracker."""
+
+DOMAIN = "waterdrop_filter_tracker"
+
+CONF_FILTER_NAME = "filter_name"
+CONF_INSTALL_DATE = "install_date"
+CONF_RATED_LIFE_DAYS = "rated_life_days"
+CONF_RATED_CAPACITY_GALLONS = "rated_capacity_gallons"
+CONF_DAILY_USAGE_GALLONS = "daily_usage_gallons"
+
+DEFAULT_FILTER_NAME = "Waterdrop Filter"
+DEFAULT_RATED_LIFE_DAYS = 180
+DEFAULT_RATED_CAPACITY_GALLONS = 400.0
+DEFAULT_DAILY_USAGE_GALLONS = 2.0
